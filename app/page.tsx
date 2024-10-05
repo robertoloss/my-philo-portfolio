c317f309eb0a8d2a88c45ba1a21b9e5e1f7d4ee7
Roberto Loss <robertoloss@gmail.com>
2024-10-05 12:57:17 +0200

    site under construction

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Cv_entry } from "@/sanity.types";
 import { getPapers } from "@/sanity/groq-queries";
 import { getCvEntry } from "@/sanity/groq-queries";
 import { PortableText, PortableTextComponents } from "next-sanity";
+import Link from "next/link";
 
 const components : PortableTextComponents = {
 	block: {
@@ -21,7 +22,20 @@ const components : PortableTextComponents = {
 export default async function Home() {
 	const list: Paper[] = await getPapers()
 	const cvEntries: Cv_entry[] = await getCvEntry()
-	return <h1>Roberto Loss</h1>
+	return (
+		<div className="flex flex-col w-full gap-y-4 h-screen justify-center items-center text-3xl font-extralight">
+			<h1 className="font-semibold text-5xl">Roberto Loss - Philosophy</h1>
+			<h1 className="mt-6 text-2xl">🚧 Site under construction 🚧</h1>
+			<h1 className="text-lg text-gray-200 mt-10">(you can find my developer website at &nbsp; 
+				<Link 
+					href="https://robertoloss.netlify.app"
+					className="text-blue-400 hover:text-blue-200 transition-all"
+				>
+					 robertoloss.netlify.app
+				</Link>)
+			</h1>
+		</div>
+	)
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24">
 			{cvEntries && cvEntries.map(entry => entry.content?.map(cvEntry =>
